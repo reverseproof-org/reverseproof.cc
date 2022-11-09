ef1815c77b167af28b3c4b4749813d692b3e7d27
orenghe988 <orenghe@gmail.com>
2022-11-09 21:35:19 +0200

    update github actions

---
+++ app/routes/index.tsx
@@ -0,0 +1,40 @@
+import { useLoaderData } from "@remix-run/react";
+
+export function loader() {
+  console.log(SUPABASE_ANON_KEY);
+  return null;
+}
+
+export default function Index() {
+  useLoaderData();
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
